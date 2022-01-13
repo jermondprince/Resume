f2be94db4fb354ef3628e4926967df35867b76b8
Jermond Prince <jermondprince@gmail.com>
2022-01-13 12:22:12 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function aboutMeOnClick() {
+  var paragraph = document.getElementById("paragraph");
+  paragraph.classList.toggle("open__dropdown");
+}
+
+function skillsOnClick() {
+  var skills = document.getElementById("icon_skills");
+  skills.classList.toggle("open__dropdown");
+}
+
+function contactOnClick() {
+  var contact = document.getElementById("icon_contact");
+  contact.classList.toggle("open__dropdown");
+}
